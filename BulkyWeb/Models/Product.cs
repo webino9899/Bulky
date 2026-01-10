66e88e24ca471be93a5b29b80a145071e849cf33
Varsha <varshakhade46@yahoo.com>
2026-01-10 15:37:24 +0530

    Add Category Property

--- BulkyWeb/Models/Product.cs
+++ BulkyWeb/Models/Product.cs
@@ -7,6 +7,8 @@
                 
             public string Description { get; set; }
             public double Price { get; set; }
-
+            public string Category { get; set; }
+        public string CategoryName { get; set; }
+        
     }
 }
